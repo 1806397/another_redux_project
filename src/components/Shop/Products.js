d8e03bb3914780dce1984d291106995b844ff1d0
Piyush Singh <1806397@kiit.ac.in>
2022-12-19 16:18:55 +0530

    3rd commit:added redux logic

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,33 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
-
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "My first book",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second Book",
+    description: "My second book",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((products) => (
+          <ProductItem
+            key={products.id}
+            id={products.id}
+            title={products.title}
+            price={products.price}
+            description={products.description}
+          />
+        ))}
       </ul>
     </section>
   );
